088bd9b8c93ee5f3ad512f40aae6c0a2ffbb7ce1
Mokhinur <mokhinur.bakhtiyorov@sinclair.edu>
2026-03-18 11:37:18 -0400

    Initial Commit

---
+++ Assingment 9/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assingment 9
+//
+//  Created by Anara B on 3/18/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
